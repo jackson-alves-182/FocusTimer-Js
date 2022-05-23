1feeeebc41915aa21881876bc76732fcc6a2f9d8
Jackson Alves <jack182@outlook.com.br>
2022-05-23 20:26:04 -0300

    fixed some details related to responsiveness

--- js/timer.js
+++ js/timer.js
@@ -39,11 +39,9 @@ export default function Timer({
          
         }
         seconds = seconds - 1;
-
         updateDisplay(minutes, seconds);
        
       }
-      
       countdown();
       
     },1000)
